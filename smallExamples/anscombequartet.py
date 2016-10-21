e5030f95bf84ef0c7c5c7ef447a638b3e3d949d3
Eric Homberger <erichomberger@gmail.com>
2016-10-20 21:15:49 -0400

    Changed commenting style

--- smallExamples/anscombequartet.py
+++ smallExamples/anscombequartet.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
-import seaborn as sns  
+import seaborn as sns
 
 sns.set(style="ticks")
 
-#load example data set
+# load example data set
 df = sns.load_dataset("anscombe")
 
-#show the results of linear regression
+# show the results of linear regression
 sns.lmplot(x = 'x', y = 'y', col = "dataset", hue = "dataset", data = df,
             col_wrap = 2, ci = None, palette = "muted", size = 4,
             scatter_kws={'s': 50, "alpha": 1})
--- smallExamples/dataCreation.py
+++ smallExamples/dataCreation.py
@@ -1,30 +1,30 @@
-#This file will contain a few functions designed to give different data distributions
+# This file will contain a few functions designed to give different data distributions
 import numpy as np
 import pandas as pd
 
 
-#randomly assign a value to each element in a series
+# randomly assign a value to each element in a series
 def randomDist(elements):
     s = pd.Series(np.random.randn(elements))
     return s
 
-#generate 'trials' random numbers between 0 and 'elements' then return a series based on frequency
+# generate 'trials' random numbers between 0 and 'elements' then return a series based on frequency
 def randomTrials(elements, trials):
-    #generate the random integers
+    # generate the random integers
     data = np.random.randint(low = 0, high = elements, size = trials)
 
-    #create a list based of the data so we can use .count() then use list comprehension to generate frequencies    
+    # create a list based of the data so we can use .count() then use list comprehension to generate frequencies
     l = list(data)
-    counts = [l.count(x) for x in range(elements)] 
+    counts = [l.count(x) for x in range(elements)]
 
-    #turn the data into a series and return it
-    s = pd.Series(counts, range(elements)) 
+    # turn the data into a series and return it
+    s = pd.Series(counts, range(elements))
     return s
 
-#roll 'dice' number of 'sides' sided 'trials' number of times
+# roll 'dice' number of 'sides' sided 'trials' number of times
 def rollDice(dice, sides, trials):
     i = 0
-    j = 0 
+    j = 0
     total = 0
     results = []
     while(i < trials):
@@ -39,5 +39,4 @@ def rollDice(dice, sides, trials):
     counts = [results.count(x) for x in range(dice, dice * sides + 1)]
     s = pd.Series(counts, range(dice, dice * sides + 1))
     return s
-
-
+    
